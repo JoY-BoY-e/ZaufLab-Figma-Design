72c7872aa21a1c0fbcceee49a4d5ac1014fcabd6
Salman Mehmood <salmanmehmood19j@gmail.com>
2025-07-31 04:10:20 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -18,7 +18,7 @@ const nextConfig: NextConfig = {
         hostname: "avatars.githubusercontent.com",
       },
     ],
-    domains: ['localhost'],
+    domains: ['localhost','images.unsplash.com', 'randomuser.me'],
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
